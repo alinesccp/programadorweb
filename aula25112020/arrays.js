8721284e2778876e10d8ab9c69ca922d8bc6a626
alinesccp <alinesccparaujo@gmail.com>
2020-11-25 16:59:16 -0300

    commit inicial

---
+++ aula25112020/arrays.js
@@ -0,0 +1,10 @@
+// Conceitos básicos sobre Arrays
+// Indice       0         1
+let frutas = ['maçã', 'banana']
+console.log(frutas. length)
+console.log(frutas)
+console.log(frutas[0])
+let adiciona = frutas.push('laranja')
+console.log(frutas[0])
+console.log(frutas.length)
+console.log(frutas)
